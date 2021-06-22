5c597c85423b8ec9745dc2a98fe34aa24f17a769
A.M. Masudul Haque (Saadi) <saadixl@outlook.com>
2021-06-22 21:15:10 +0800

    Schedule the publisher every 6 hours using a cron

---
+++ services/publisher/constants.js
@@ -0,0 +1,5 @@
+const CRON_PATTERN_EVERY_SIX_HOURS = '0 */6 * * *';
+
+module.exports = {
+    CRON_PATTERN_EVERY_SIX_HOURS,
+};
--- services/publisher/index.js
+++ services/publisher/index.js
@@ -1,4 +1,6 @@
 const redis = require("redis");
+const { CRON_PATTERN_EVERY_SIX_HOURS } = require('./constants');
+const CronJob = require('cron').CronJob;
 const { printLog } = require('./utils');
 
 const redisClient = redis.createClient({
@@ -6,8 +8,12 @@ const redisClient = redis.createClient({
     port: 6379
 });
 
-setInterval(() => {
+printLog('Publisher started. In 6 hours it will start publishing.');
+
+const job = new CronJob(CRON_PATTERN_EVERY_SIX_HOURS, async () => {
     redisClient.publish('send_newsfeed_update', 'Hey subscirber! Send the latest news to sir.', () => {
-        printLog('send_newsfeed_update message sent');
+        printLog('send_newsfeed_update message sent, next publish is scheduled 6 hours later.');
     });
-}, 10000);
+}, null, true, 'Asia/Singapore');
+
+job.start();
