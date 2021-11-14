72a1a3f826b117cc069ebac6c14de8b78b31cfdf
Saadi <saadixl@outlook.com>
2021-11-14 21:55:43 +0800

    Add test code in action.js

--- action.js
+++ action.js
@@ -1,1 +1,15 @@
+const { exec } = require("child_process");
+
 console.log("Action.js fired");
+
+exec("ls -l", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
