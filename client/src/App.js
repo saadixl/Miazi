3ca689972bae0e37ceeb291e222baf4c8c1d1cd5
A.M. Masudul Haque (Saadi) <saadixl@outlook.com>
2022-02-01 00:31:06 +0800

    Create basic dashboard layout with mui.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,339 @@
+import * as React from 'react';
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Switch from '@mui/material/Switch';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
-function App() {
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1976d2'
+    },
+    secondary: {
+      main: '#121212'
+    }
+  },
+});
+
+export function ButtonAppBar() {
   return (
-    <div className="App">
-      Hello, World
+    <Box sx={{ flexGrow: 1 }}>
+      <ThemeProvider theme={darkTheme}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Miazi Console
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
+    </Box>
+  );
+}
+
+export function AlignItemsList() {
+  return (
+    <List sx={{ width: '100%', minHeight: '80vh'}}>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Brunch this weekend?"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                Ali Connors
+              </Typography>
+              {" — I'll be in your neighborhood doing errands this…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Summer BBQ"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                to Scott, Alex, Jennifer
+              </Typography>
+              {" — Wish I could come, but I'm out of town this…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Oui Oui"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                Sandra Adams
+              </Typography>
+              {' — Do you have Paris recommendations? Have you ever…'}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Summer BBQ"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                to Scott, Alex, Jennifer
+              </Typography>
+              {" — Wish I could come, but I'm out of town this…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Summer BBQ"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                to Scott, Alex, Jennifer
+              </Typography>
+              {" — Wish I could come, but I'm out of town this…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Summer BBQ"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                to Scott, Alex, Jennifer
+              </Typography>
+              {" — Wish I could come, but I'm out of town this…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+    </List>
+  );
+}
+
+function GridItemTitle(props) {
+  return (<Typography 
+    variant="h7"
+    align="left"
+    component="p"
+    sx={{
+      padding: '10px 15px',
+      fontWeight: 'bold'
+    }}
+  >
+    {props.children}
+  </Typography>);
+};
+
+export function AlertDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Enable Warden mode
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Do you want to enable Warden mode?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Once Warden mode is enabled, upon not checking in, you emergency contact will receive an email from you.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose} autoFocus>
+            Enable
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Container maxWidth="xl" disableGutters={true}>
+      <ButtonAppBar/>
+      <ThemeProvider theme={darkTheme}>
+      <Box sx={{ flexGrow: 1, marginTop: '10px', padding: '10px 25px'  }}>
+        <Grid container spacing={2}>
+          <Grid item xs={1}></Grid>
+          <Grid item xs={6}>
+            <Item>
+              <GridItemTitle>Newsfeed</GridItemTitle>
+              <AlignItemsList/>
+            </Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>
+              <GridItemTitle>Newsfeed settings</GridItemTitle>
+              <Box sx={{ padding: '5px 15px', textAlign: 'left' }}>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Refresh frquency</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      
+                      label="Refresh frquency"
+                    >
+                      <MenuItem value={10}>1 hour</MenuItem>
+                      <MenuItem value={20}>2 hours</MenuItem>
+                      <MenuItem value={30}>4 hours</MenuItem>
+                      <MenuItem value={30}>6 hours</MenuItem>
+                      <MenuItem value={30}>8 hours</MenuItem>
+                      <MenuItem value={30}>12 hours</MenuItem>
+                      <MenuItem value={30}>1 day</MenuItem>
+                    </Select>
+                </FormControl>
+              </Box>
+              <Box sx={{ padding: '5px 15px', textAlign: 'left' }}>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Email frquency</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      
+                      label="Email frquency"
+                    >
+                      <MenuItem value={10}>1 hour</MenuItem>
+                      <MenuItem value={20}>2 hours</MenuItem>
+                      <MenuItem value={30}>4 hours</MenuItem>
+                      <MenuItem value={30}>6 hours</MenuItem>
+                      <MenuItem value={30}>8 hours</MenuItem>
+                      <MenuItem value={30}>12 hours</MenuItem>
+                      <MenuItem value={30}>1 day</MenuItem>
+                    </Select>
+                </FormControl>
+              </Box>
+              <GridItemTitle>Warden</GridItemTitle>
+              <Box sx={{ padding: '5px 15px', textAlign: 'left' }}>
+                <AlertDialog/>
+              </Box>
+            </Item>
+          </Grid>
+        </Grid>
+        </Box>
+        </ThemeProvider>
+    </Container>
+  );
+}
