2400530b5c812f477d972df673414482bbea5450
A.M. Masudul Haque (Saadi) <saadixl@outlook.com>
2022-03-19 23:51:41 +0800

    Update domain

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -1,2 +1,2 @@
-const API_DOMAIN = 'http://localhost:3004';
+const API_DOMAIN = 'http://mh7.pw:3004';
 export const NEWSFEED_API_URL = `${API_DOMAIN}/list_news`;
--- services/router/index.js
+++ services/router/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 80;
 
 app.get('/', (req, res) => {
-    res.send(`Hello World! The server date-time is now ${new Date().toString()}.\nThis domain is used for Software Engineering experiments only. This message is sent from the router service.`);
+    res.send(`Hello World!<br/>The server date-time is now ${new Date().toString()}.<br/>This domain is used for Software Engineering experiments only. This message is sent from the router service.`);
 });
 
 app.listen(port, () => {
