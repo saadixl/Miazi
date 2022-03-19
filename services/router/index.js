84c760f33f59e3e6c5bf88015e5a7d61ec119b56
A.M. Masudul Haque (Saadi) <saadixl@outlook.com>
2022-03-20 00:34:20 +0800

    Update index.js

--- services/router/index.js
+++ services/router/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+var request = require('request');
 const app = express();
 const port = 80;
 
 app.get('/', (req, res) => {
     res.send(`Hello World!<br/>The server date-time is now ${new Date().toString()}.<br/>This domain is used for Software Engineering experiments only. This message is sent from the router service.`);
+});
+
+app.get('/webapp', (req, res) => {
+    request.get('http://webapp:3000', function(err, response, body) {
+        if (!err) {
+            req.send(body);
+        }
+    });
 });
 
 app.listen(port, () => {
